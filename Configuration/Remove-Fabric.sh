29427a332b4d57c3d18e6b6384a2f546f580daf9
Sudhishna <ssendhil@juniper.net>
2018-03-06 14:40:21 -0800

    Update Remove-Fabric.sh

--- Configuration/Remove-Fabric.sh
+++ Configuration/Remove-Fabric.sh
@@ -3,9 +3,14 @@
 # Comannd example ./Remove-Fabric.sh
 # Date written 2018 February 12
 
+x=`sed -n '1p' ~/BuildSystem/PodInfo.txt`;
+echo "$x";
+PodNumber=$(echo $x | cut -d":" -f2);
+echo $PodNumber
+
 printf  "Removing configuration begins.\r"
 
-ansible-playbook -i 01-Interfaces/all.inv 01-Interfaces/remove-config.yml --extra-vars "pod=pod1"
+ansible-playbook -i 01-Interfaces/all.inv 01-Interfaces/remove-config.yml --extra-vars "pod=pod$PodNumber"
 sleep 5 
 
 printf "\n\nThe configurations were successfully removed.\n"
