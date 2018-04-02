3e44e609b522678a456878897bc5ee537d743eef
Sudhishna <ssendhil@juniper.net>
2018-04-01 17:45:21 -0700

    Update Build-Fabric.sh

--- Configuration/Build-Fabric.sh
+++ Configuration/Build-Fabric.sh
@@ -10,23 +10,23 @@ echo $PodNumber
 
 printf  "Fabric initialization begins.\r"
 
-ansible-playbook -i 01-Interfaces/all.inv 01-Interfaces/config-interfaces.yml --extra-vars "pod=pod$PodNumber"
+ansible-playbook -i all.inv 01-Interfaces/config-interfaces.yml --extra-vars "pod=pod$PodNumber"
 printf  "Interfaces Configured.\r"
 sleep 3
 
-ansible-playbook -i 02-Scale-Options/all.inv 02-Scale-Options/config-scaleoptions.yml --extra-vars "pod=pod$PodNumber"
+ansible-playbook -i all.inv 02-Scale-Options/config-scaleoptions.yml --extra-vars "pod=pod$PodNumber"
 printf  "Scale Options Configured.\r"
 sleep 3
 
-ansible-playbook -i 03-Routing-iBGP/all.inv 03-Routing-iBGP/config-routing.yml --extra-vars "pod=pod$PodNumber"
+ansible-playbook -i all.inv 03-Routing-iBGP/config-routing.yml --extra-vars "pod=pod$PodNumber"
 printf  "Routing options set.\r"
 sleep 3
 
-ansible-playbook -i 04-Underlay-OSPF/all.inv 04-Underlay-OSPF/config-underlay.yml --extra-vars "pod=pod$PodNumber"
+ansible-playbook -i all.inv 04-Underlay-OSPF/config-underlay.yml --extra-vars "pod=pod$PodNumber"
 printf  "OSPF Routing Protocol installed and iBGP configured.\r"
 sleep 3
 
-ansible-playbook -i 05-Overlay/all.inv 05-Overlay/config-overlay.yml --extra-vars "pod=pod$PodNumber"
+ansible-playbook -i all.inv 05-Overlay/config-overlay.yml --extra-vars "pod=pod$PodNumber"
 printf  "Overlay deployed.\r"
 sleep 3
 
